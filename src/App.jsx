f7b81807a1e33e267568652f4ccabf54ac9fc5cd
ranji_win_25 <ranjitsbatra@yahoo.com>
2025-09-22 13:14:56 -0400

    installing amplify Atal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
-
-
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 function App() {
@@ -12,39 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={reactLogo} className="logo react" alt="React logo" />
-        <h1>Hello from Amplify</h1>
+        <h1>Hello from Amplify by Atal Cloud</h1>
       </header>
     </div>
   );
 }
-
-
-
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
